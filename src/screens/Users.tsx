f2509bb9280e6f845ea06baa65458ebbddc0226a
ugo <ugodelucia@gmail.com>
2024-03-02 23:01:19 +0100

    added axios

--- src/screens/Users.tsx
+++ src/screens/Users.tsx
@@ -1,7 +1,13 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native"
 
 const Users = () => {
 
+
+    useEffect(()=>{
+
+    },[]);
+    
     return (
         <View>
             <Text>Users</Text>
